99d6da36d9d3fc31b0c1fdddd6782297e5d20fe5
DerekHsieh032 <legokered@icloud.com>
2022-12-19 13:45:58 -0800

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Derek Hsieh on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
